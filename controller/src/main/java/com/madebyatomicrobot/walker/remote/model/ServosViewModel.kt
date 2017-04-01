64aced6f4bf39fd0b8f8278bc40811455311900d
Patrick Hammond <patrick@madebyatomicrobot.com>
2017-03-31 23:51:33 -0400

    Preparing for remote configuration.

--- controller/src/main/java/com/madebyatomicrobot/walker/remote/model/ServosViewModel.kt
+++ controller/src/main/java/com/madebyatomicrobot/walker/remote/model/ServosViewModel.kt
@@ -4,6 +4,7 @@ import android.databinding.BaseObservable
 import android.databinding.Bindable
 import android.support.v4.app.FragmentActivity
 import android.util.Log
+import android.view.View
 import com.madebyatomicrobot.walker.connector.data.RemoteConnector
 import com.madebyatomicrobot.walker.connector.data.ServosConfig
 import com.madebyatomicrobot.walker.connector.data.ServosStatus
@@ -44,13 +45,7 @@ class ServosViewModel(val activity: FragmentActivity, val connector: RemoteConne
         disposables.clear()
     }
 
-    fun servoLabel(servo: String, value: Int): String {
-        return "$servo ($value${kotlin.text.Typography.degree})"
-    }
-
-    @Bindable fun isReadOnly(): Boolean {
-        return !isControlServos() && !isWatchServos()
-    }
+    @Bindable fun isReadOnly(): Boolean = !isControlServos() && !isWatchServos()
 
     fun isControlServos(): Boolean {
         return servosConfig.global.controlServos
@@ -59,9 +54,7 @@ class ServosViewModel(val activity: FragmentActivity, val connector: RemoteConne
     fun setControlServos(controlServos: Boolean) {
         servosConfig.global.controlServos = controlServos
         servosConfig.global.watchServos = false
-        notifyChange()
-
-        connector.setServosConfig(servosConfig)
+        saveServoConfig()
     }
 
     fun isWatchServos(): Boolean {
@@ -71,27 +64,51 @@ class ServosViewModel(val activity: FragmentActivity, val connector: RemoteConne
     fun setWatchServos(watchServos: Boolean) {
         servosConfig.global.controlServos = false
         servosConfig.global.watchServos = watchServos
-        notifyChange()
+        saveServoConfig()
+    }
 
+    private fun saveServoConfig() {
+        notifyChange()
         connector.setServosConfig(servosConfig)
     }
 
-    @Bindable fun isServo00Enabled(): Boolean = !isReadOnly() && servosConfig.servo00.enabled
-    @Bindable fun isServo01Enabled(): Boolean = !isReadOnly() && servosConfig.servo01.enabled
-    @Bindable fun isServo02Enabled(): Boolean = !isReadOnly() && servosConfig.servo02.enabled
-    @Bindable fun isServo03Enabled(): Boolean = !isReadOnly() && servosConfig.servo03.enabled
-    @Bindable fun isServo04Enabled(): Boolean = !isReadOnly() && servosConfig.servo04.enabled
-    @Bindable fun isServo05Enabled(): Boolean = !isReadOnly() && servosConfig.servo05.enabled
-    @Bindable fun isServo06Enabled(): Boolean = !isReadOnly() && servosConfig.servo06.enabled
-    @Bindable fun isServo07Enabled(): Boolean = !isReadOnly() && servosConfig.servo07.enabled
-    @Bindable fun isServo08Enabled(): Boolean = !isReadOnly() && servosConfig.servo08.enabled
-    @Bindable fun isServo09Enabled(): Boolean = !isReadOnly() && servosConfig.servo09.enabled
-    @Bindable fun isServo10Enabled(): Boolean = !isReadOnly() && servosConfig.servo10.enabled
-    @Bindable fun isServo11Enabled(): Boolean = !isReadOnly() && servosConfig.servo11.enabled
-    @Bindable fun isServo12Enabled(): Boolean = !isReadOnly() && servosConfig.servo12.enabled
-    @Bindable fun isServo13Enabled(): Boolean = !isReadOnly() && servosConfig.servo13.enabled
-    @Bindable fun isServo14Enabled(): Boolean = !isReadOnly() && servosConfig.servo14.enabled
-    @Bindable fun isServo15Enabled(): Boolean = !isReadOnly() && servosConfig.servo15.enabled
+    @Bindable fun getServo00Label(): String = getServoLabel("00", servosStatus.servo00)
+    @Bindable fun getServo01Label(): String = getServoLabel("01", servosStatus.servo01)
+    @Bindable fun getServo02Label(): String = getServoLabel("02", servosStatus.servo02)
+    @Bindable fun getServo03Label(): String = getServoLabel("03", servosStatus.servo03)
+    @Bindable fun getServo04Label(): String = getServoLabel("04", servosStatus.servo04)
+    @Bindable fun getServo05Label(): String = getServoLabel("05", servosStatus.servo05)
+    @Bindable fun getServo06Label(): String = getServoLabel("06", servosStatus.servo06)
+    @Bindable fun getServo07Label(): String = getServoLabel("07", servosStatus.servo07)
+    @Bindable fun getServo08Label(): String = getServoLabel("08", servosStatus.servo08)
+    @Bindable fun getServo09Label(): String = getServoLabel("09", servosStatus.servo09)
+    @Bindable fun getServo10Label(): String = getServoLabel("10", servosStatus.servo10)
+    @Bindable fun getServo11Label(): String = getServoLabel("11", servosStatus.servo11)
+    @Bindable fun getServo12Label(): String = getServoLabel("12", servosStatus.servo12)
+    @Bindable fun getServo13Label(): String = getServoLabel("13", servosStatus.servo13)
+    @Bindable fun getServo14Label(): String = getServoLabel("14", servosStatus.servo14)
+    @Bindable fun getServo15Label(): String = getServoLabel("15", servosStatus.servo15)
+
+    private fun getServoLabel(id: String, servo: ServosStatus.Servo): String = "$id (${servo.position}${kotlin.text.Typography.degree})"
+
+    @Bindable fun isServo00Enabled(): Boolean = isServoEnabled(servosConfig.servo00)
+    @Bindable fun isServo01Enabled(): Boolean = isServoEnabled(servosConfig.servo01)
+    @Bindable fun isServo02Enabled(): Boolean = isServoEnabled(servosConfig.servo02)
+    @Bindable fun isServo03Enabled(): Boolean = isServoEnabled(servosConfig.servo03)
+    @Bindable fun isServo04Enabled(): Boolean = isServoEnabled(servosConfig.servo04)
+    @Bindable fun isServo05Enabled(): Boolean = isServoEnabled(servosConfig.servo05)
+    @Bindable fun isServo06Enabled(): Boolean = isServoEnabled(servosConfig.servo06)
+    @Bindable fun isServo07Enabled(): Boolean = isServoEnabled(servosConfig.servo07)
+    @Bindable fun isServo08Enabled(): Boolean = isServoEnabled(servosConfig.servo08)
+    @Bindable fun isServo09Enabled(): Boolean = isServoEnabled(servosConfig.servo09)
+    @Bindable fun isServo10Enabled(): Boolean = isServoEnabled(servosConfig.servo10)
+    @Bindable fun isServo11Enabled(): Boolean = isServoEnabled(servosConfig.servo11)
+    @Bindable fun isServo12Enabled(): Boolean = isServoEnabled(servosConfig.servo12)
+    @Bindable fun isServo13Enabled(): Boolean = isServoEnabled(servosConfig.servo13)
+    @Bindable fun isServo14Enabled(): Boolean = isServoEnabled(servosConfig.servo14)
+    @Bindable fun isServo15Enabled(): Boolean = isServoEnabled(servosConfig.servo15)
+
+    private fun isServoEnabled(servo: ServosConfig.Servo): Boolean = !isReadOnly() && servo.enabled
 
     fun setServo(servo: Int, progress: Int) {
         servoFunctions[servo].invoke(progress)
@@ -128,7 +145,24 @@ class ServosViewModel(val activity: FragmentActivity, val connector: RemoteConne
         }
     }
 
-    fun editServo(servoId: String) {
+    fun editServo00(view: View) = editServo("00")
+    fun editServo01(view: View) = editServo("01")
+    fun editServo02(view: View) = editServo("02")
+    fun editServo03(view: View) = editServo("03")
+    fun editServo04(view: View) = editServo("04")
+    fun editServo05(view: View) = editServo("05")
+    fun editServo06(view: View) = editServo("06")
+    fun editServo07(view: View) = editServo("07")
+    fun editServo08(view: View) = editServo("08")
+    fun editServo09(view: View) = editServo("09")
+    fun editServo10(view: View) = editServo("10")
+    fun editServo11(view: View) = editServo("11")
+    fun editServo12(view: View) = editServo("12")
+    fun editServo13(view: View) = editServo("13")
+    fun editServo14(view: View) = editServo("14")
+    fun editServo15(view: View) = editServo("15")
+
+    private fun editServo(servoId: String) {
         val fragment = ServoEditorFragment.newInstance(servoId)
         fragment.show(activity.supportFragmentManager, null)
     }
